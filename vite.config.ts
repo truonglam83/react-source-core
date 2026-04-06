bea402dcd0070bb81cf462c3ce649826295ec4b2
truonglam83 <truonglam83.dev@gmail.com>
2026-04-06 21:34:51 +0700

    config src update

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      additionalData: `
+        @import "@/styles/variables.scss";
+        @import "@/styles/mixins.scss";
+      `,
     },
   },
 });
